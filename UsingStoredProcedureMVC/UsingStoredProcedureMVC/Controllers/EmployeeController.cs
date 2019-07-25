30e82b6be6f0578b118e45f717ca4d2c91bdf1b9
sanshanthan <sanshanthan@yahoo.com>
2019-07-25 10:30:26 +0530

    Stored Procedure In Entity Framework MVC

---
+++ UsingStoredProcedureMVC/UsingStoredProcedureMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UsingStoredProcedureMVC.Models;
+
+namespace UsingStoredProcedureMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        Emp_Model mod = new Emp_Model();
+        public ActionResult Index()
+        {
+            List<getAllDetails_Result> result = mod.GetEmployees();
+            return View(result);
+        }
+    }
+}
---
+++ UsingStoredProcedureMVC/UsingStoredProcedureMVC/Models/Emp_Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UsingStoredProcedureMVC.Models
+{
+    public class Emp_Model
+    {
+        ABC_Company_DBEntities db = new ABC_Company_DBEntities();
+        public List<getAllDetails_Result> GetEmployees()
+        {
+            return db.getAllDetails().ToList();
+        }
+    }
+}
