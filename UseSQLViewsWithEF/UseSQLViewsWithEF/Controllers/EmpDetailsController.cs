6629c03c83c151d811094310af7b38f830c312d4
sanshanthan <sanshanthan@yahoo.com>
2019-07-26 10:02:03 +0530

    NotWorking

---
+++ UseSQLViewsWithEF/UseSQLViewsWithEF/Controllers/EmpDetailsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UseSQLViewsWithEF.Models;
+
+namespace UseSQLViewsWithEF.Controllers
+{
+    public class EmpDetailsController : Controller
+    {
+        private ABC_Company_DBEntities2 db = new ABC_Company_DBEntities2();
+
+        public  ObjectResult<getAllDetails_Result> getAllDetails()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<getAllDetails_Result>("getAllDetails");
+        }
+        // GET: EmpDetails
+        public ActionResult Index()
+        {
+            EmpDetailsController emc = new EmpDetailsController();
+            return View(db.EmpDetails.ToList());
+        }
+    }
+}
---
+++ UseSQLViewsWithEF/UseSQLViewsWithEF/Controllers/SqlViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UseSQLViewsWithEF.Models;
+
+namespace UseSQLViewsWithEF.Controllers
+{
+    public class SqlViewController : Controller
+    {
+        // GET: SqlView
+        public ActionResult Index()
+        {
+            ABC_Company_DBEntities2 ctx = new ABC_Company_DBEntities2();
+            var result = ctx.EmpDetails;
+            return View(result); ;
+        }
+
+        //string bccc = null;
+
+        //    using (ABC_Company_DBEntities2 ctx = new ABC_Company_DBEntities2())
+        //    {
+        //        var result = from t in ctx.EmpDetails
+        //                     select t;
+        //        foreach (var t in result)
+        //        {
+        //            bccc = t.Developer_Name;
+        //        }
+        //    }
+        //    return View(bccc);
+    }
+}
